774b8fcfe30adc75d5731eddb24d72ba65528d43
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-09-24 18:50:10 +0200

    add missing exporters

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 require 'extends/proposal_serializer_extend'
-#require 'extends/comment_serializer_extend'
 require 'decidim/comments/comment_serializer'
+require 'decidim/exporters/csv'
+require 'decidim/exporters/json'
+require 'decidim/exporters/exporter'
