8b43a4f347ebaca9c458eb79b8243965052b6baa
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-09-24 17:37:07 +0200

    import missing factories

--- lib/decidim/greenpeace_overload/test/factories.rb
+++ lib/decidim/greenpeace_overload/test/factories.rb
@@ -2,6 +2,7 @@
 
 require "decidim/core/test/factories"
 require "decidim/proposals/test/factories"
+require "decidim/comments/test/factories"
 
 FactoryBot.define do
   factory :greenpeace_overload_component, parent: :component do
